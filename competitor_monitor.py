dc9a743da1d7824f0268c044cc4d077d963c2f04
YourName <behappywillyou@gmail.com>
2026-04-14 11:53:59 +0800

    新增組員email

--- competitor_monitor.py
+++ competitor_monitor.py
@@ -15,7 +15,10 @@ EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")  # GitHub Secrets 中的 16 �
 
 # 更新收件人名單
 EMAIL_RECEIVERS = [
-    "jenny_lee@digifinex.org"
+    "jenny_lee@digifinex.org",
+    "vic_lu@digifinex.org",
+    "rhett_fang@digifinex.org",
+    "ron_ni@digifinex.org"
 ]
 
 # 初始化新版 SDK client
